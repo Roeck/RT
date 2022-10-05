447f59ae0d97da7c997750e42480a271e767d2ca
rons9n <rons9n@gmail.com>
2022-10-04 19:41:53 -0500

    Initial App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,18 @@ import './App.css';
 function App() {
   return (
     <React.Fragment>
-      <h2 className="text-warning">App component</h2>
-      <i className="fa fa-home fa-2x" />
+      <div className='container mt-3'>
+        <div className='grid'>
+          <div className='row'>
+            <div className='col'></div>
+            <p className='h3 fw-bold text-primary'>App component</p>
+            <p className='fst-italic'>Lorem ipsum</p>
+            <button className='btn btn-primary btn-sm'>
+              <i className='fa fa-book' />Read more
+            </button>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
 
   );
