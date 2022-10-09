ae5aa1f944bb0369495a1cb8e8672413940614a7
rons9n <rons9n@gmail.com>
2022-10-08 20:45:02 -0500

    Main component update

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Customer from "./components/Customer";
+import {CustomerClass} from "./components/CustomerClass";
+import Employee from "./components/Employee";
+import {EmployeeClass} from "./components/EmployeeClass";
 
 function App() {
   return (
     <React.Fragment>
-      <div className='container mt-3'>
-        <div className='grid'>
-          <div className='row'>
-            <div className='col'></div>
-            <p className='h3 fw-bold text-primary'>App component</p>
-            <p className='fst-italic'>Lorem ipsum</p>
-            <button className='btn btn-primary btn-sm'>
-              <i className='fa fa-book' />Read more
-            </button>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='col'>
-            <Customer name={'Ron'} age={33} title={'dev'} />
-          </div>
-        </div>
-      </div>
-    </React.Fragment >
+       <div className="container mt-3">
+           <div className="grid">
+               <div className="row">
+                   <div className="col">
+                       <p className="h3 fw-bold text-success">App Component</p>
+                       <p className="fst-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ducimus facilis nihil numquam quas quia quo repellat! Alias consequatur cumque deleniti dicta, dolores, eum eveniet facilis minima nisi quia quod.</p>
+                    <button className="btn btn-success btn-sm">
+                       <i className="fa fa-book"/> Read More</button>
+                   </div>
+               </div>
+
+               {/*<div className="row">
+                   <div className="col">
+                       <Customer name={'Rajan'} age={25} title={'Software Engineer'}/>
+                   </div>
+               </div>
+
+               <div className="row">
+                   <div className="col">
+                       <CustomerClass name={'Rajan'} age={25} title={'Software Engineer'}/>
+                   </div>
+               </div>*/}
+
+               <div className="row">
+                   <div className="col">
+                       <Employee/>
+                   </div>
+               </div>
+
+               <div className="row">
+                   <div className="col">
+                       <EmployeeClass/>
+                   </div>
+               </div>
+           </div>
+       </div>
+    </React.Fragment>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 /* Font Awesome */
-import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+/* Bootstrap */
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
